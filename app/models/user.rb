2ef3a70f5ef90be15db7dcfe69c7bf22349bf03e
Moe Ogunkeye <moe.ogunkeye@oit.edu>
2021-12-06 16:33:47 -0800

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base 
+    validates :username, presence: true, uniquenss: true, { case_sensitive: false}, length: {minimum: 3, maximum: 25 }
+    validates :email, presence: true, uniquenss: true
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-.]+\.[a-z]+\z/i/
+    validates :email, presence: true, length: { maximum: 105 },
+    uniquenss: { case_sensitive: false },
+    format: { with: VALID_EMAIL_REGEX }
+    before_save { self.email = email.downcase }
+    
 end
