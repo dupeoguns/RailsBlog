a5034c3f02cf670e3b41e1df74d92990f8da6f68
dupeoguns <dupeoguns@gmail.com>
2023-01-31 11:11:54 -0800

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ActiveRecord::Base
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
     validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
-    validates :password, presence: true, length: { minimum: 6, maximum: 25 }
+    validates :password_digest, presence: true, length: { minimum: 6, maximum: 25 }
     
     before_save { self.email = email.downcase }
 
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_120745) do
+ActiveRecord::Schema.define(version: 2021_12_17_002355) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2021_12_16_120745) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
